aaaf6ec2497c51e1862f29e174e7a628a1798173
Maryna Sheihets <maryna.sheihets@texthelp.com>
2024-07-15 16:15:36 +0300

    feat: create initial structure

---
+++ loader.js
@@ -0,0 +1,43 @@
+new class {
+    constructor(){
+        debugger;
+        const target = document.querySelector('HTML');
+        const observerSettings = {
+            subtree: true,
+            childList: true,
+        };
+
+        const callback = (mutationList, observer) => {
+            if (document.body && window === top) {
+               
+                        this.#loadScript("top.js");
+                    
+            } 
+                observer.disconnect();
+        }
+
+        const observer = new MutationObserver(callback);
+        observer.observe(target, observerSettings);
+
+    }
+
+    #loadScript(path) {
+        const parentElement = document.documentElement;
+
+        let scriptElement = document.createElement("script");
+        scriptElement.type = "text/javascript";
+        scriptElement.src = chrome.runtime.getURL(path);
+        scriptElement.setAttribute("charset", "UTF-8");
+        scriptElement.setAttribute("defer", "defer");
+        parentElement.appendChild(scriptElement);
+
+        if (document.head) {
+            document.head.appendChild(scriptElement);
+        }
+        else {
+            document.documentElement.appendChild(scriptElement);
+        }
+    }
+
+    
+}
---
+++ src/top.index.ts
@@ -0,0 +1,29 @@
+import { container, inject, singleton } from "tsyringe";
+import { ShadowWrapper } from "./components/wrappers/shadow-wrapper.component";
+
+
+@singleton()
+class Entry {
+    constructor(
+        @inject("Shadow") shadow: ShadowRoot,
+    ) {
+        const element = document.createElement('div');
+        element.classList.add("wordWander")
+        shadow.append(element);
+
+        document.body.append(shadowWrapper);
+
+    }
+}
+
+
+customElements.define('word-wander', ShadowWrapper)
+const shadowWrapper = new ShadowWrapper();
+
+const shadow = shadowWrapper.attachShadow({ mode: 'closed' });
+
+container.register('Shadow', {
+    useValue: shadow,
+});
+
+container.resolve(Entry);
