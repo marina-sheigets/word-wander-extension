b6cb0fb9316d843fada3ecb397e2a998c2d17df5
Maryna Sheihets <maryna.sheihets@texthelp.com>
2024-08-07 21:53:06 +0300

    refactor: word-translation component

--- src/components/word-translation/word-translation.component.ts
+++ src/components/word-translation/word-translation.component.ts
@@ -2,8 +2,6 @@ import { injectable } from "tsyringe";
 import { BaseComponent } from "../base-component/base-component";
 import * as styles from './word-translation.component.css';
 import { IconService } from "../../services/icon/icon.component";
-import englishFlag from '../../../assets/flags/flag_809.png';
-import ukrainianFlag from '../../../assets/flags/flag_422.png';
 
 @injectable()
 export class WordTranslationComponent extends BaseComponent {
@@ -15,25 +13,17 @@ export class WordTranslationComponent extends BaseComponent {
     }
 
     addPair(sourceWord: string, targetWord: string) {
-        const wordFlag = document.createElement('img');
-        const translationFlag = document.createElement('img');
         const word = document.createElement('div');
         const translation = document.createElement('div');
         const arrow = document.createElement('div');
-
-        wordFlag.src = englishFlag;
-        wordFlag.width = 35;
-        translationFlag.src = ukrainianFlag;
-        translationFlag.width = 35;
 
         word.textContent = sourceWord;
         translation.textContent = targetWord;
         arrow.append(this.iconService.init('arrow_forward'));
 
-        this.rootElement.append(wordFlag,
+        this.rootElement.append(
             word,
             arrow,
-            translationFlag,
             translation
         );
 
