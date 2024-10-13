562d9a825f95d64ba75a7b7780c971221f912b31
marina-sheigets <sejmar1705@gmail.com>
2024-10-13 22:25:35 +0300

    feat: add timer component

---
+++ src/components/timer/timer.component.ts
@@ -0,0 +1,60 @@
+import { injectable } from "tsyringe";
+import { Informer } from "../../services/informer/informer.service";
+import { IconName } from "../../types/IconName";
+import { wait } from "../../utils/wait";
+import { BaseComponent } from "../base-component/base-component";
+import { IconComponent } from "../icon/icon.component";
+import * as styles from './timer.component.css';
+
+@injectable()
+export class TimerComponent extends BaseComponent {
+    private content = document.createElement('div');
+    private timer = document.createElement('div');
+    private interval: NodeJS.Timeout | null = null;
+    public onTimerEnd = new Informer();
+
+    constructor() {
+        super(styles);
+
+        this.content.classList.add(styles.content);
+        this.timer.classList.add(styles.timer);
+
+        this.content.append(this.timer);
+        this.rootElement.append(this.content);
+
+        this.onTimerEnd.subscribe(() => { this.interrupt(false); });
+    }
+
+    start(seconds: number) {
+        this.timer.textContent = seconds.toString();
+        this.rootElement.classList.remove(styles.failed);
+
+        this.interval = setInterval(() => {
+            seconds--;
+
+            if (seconds <= 0) {
+                clearInterval(this.interval as NodeJS.Timeout);
+                this.timer.textContent = '0';
+                this.onTimerEnd.inform();
+            } else {
+                this.timer.textContent = seconds.toString();
+            }
+        }, 1000);
+    }
+
+    public async interrupt(isSuccess: boolean, timeout: number = 0) {
+        this.timer.textContent = '';
+        clearInterval(this.interval as NodeJS.Timeout);
+        if (isSuccess) {
+            const tickSVG = new IconComponent();
+            tickSVG.setIcon(IconName.Tick);
+            this.timer.append(tickSVG.rootElement);
+            await wait(timeout);
+        } else {
+            const crossSVG = new IconComponent();
+            crossSVG.setIcon(IconName.Close);
+            this.timer.append(crossSVG.rootElement);
+            this.rootElement.classList.add(styles.failed);
+        }
+    }
+}
