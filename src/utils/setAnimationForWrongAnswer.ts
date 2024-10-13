23959f91987bf9e0b6e8d376613e188b44eb6b2c
marina-sheigets <sejmar1705@gmail.com>
2024-10-13 22:25:49 +0300

    refactor: utils

--- src/utils/setAnimationForWrongAnswer.ts
+++ src/utils/setAnimationForWrongAnswer.ts
@@ -1,9 +1,9 @@
-export function setAnimationForWrongAnswer(target: HTMLButtonElement, styles: { [key: string]: string }): Promise<void> {
+export function setAnimationForWrongAnswer(target: HTMLButtonElement, styles: { [key: string]: string }, ms: number = 300): Promise<void> {
     return new Promise((resolve) => {
         target.classList.add(styles.wrongAnswer);
         setTimeout(() => {
             target.classList.remove(styles.wrongAnswer);
             resolve();
-        }, 300);
+        }, ms);
     });
 }
