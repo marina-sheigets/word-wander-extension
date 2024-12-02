8d6b52267add0ba8d6dbafacd8d6c162b96c2b53
marina-sheigets <sejmar1705@gmail.com>
2024-12-02 20:16:51 +0200

    fix: close menu when misclick

--- src/components/menu/menu.component.ts
+++ src/components/menu/menu.component.ts
@@ -4,11 +4,16 @@ import * as styles from './menu.component.css';
 
 @injectable()
 export class MenuComponent extends BaseComponent {
+    private innerMouseDownEvent: MouseEvent | null = null;
+
     constructor() {
         super(styles);
 
         this.hide = this.hide.bind(this);
         this.open = this.open.bind(this);
+
+        this.rootElement.addEventListener("mouseup", (e) => { e.stopImmediatePropagation(), true });
+        this.rootElement.addEventListener("mousedown", this.onMouseDown.bind(this))
 
         this.hide();
     }
@@ -16,9 +21,11 @@ export class MenuComponent extends BaseComponent {
 
     hide() {
         this.rootElement.classList.add(styles.hidden);
+        document.removeEventListener('mousedown', this.onDocumentMouseDown);
     }
 
     open() {
+        document.addEventListener('mousedown', this.onDocumentMouseDown);
         this.rootElement.classList.remove(styles.hidden);
     }
 
@@ -28,5 +35,19 @@ export class MenuComponent extends BaseComponent {
         } else {
             this.hide()
         }
+    }
+
+    protected onMouseDown(event: MouseEvent) {
+        if (!this.rootElement.classList.contains(styles.hidden)) {
+            this.innerMouseDownEvent = event;
+        }
+    }
+
+    private onDocumentMouseDown = () => {
+        if (this.innerMouseDownEvent === null) {
+            this.hide();
+        }
+
+        this.innerMouseDownEvent = null;
     }
 }
