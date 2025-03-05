c6a5a654f3bd7f309802680d9964560de511a39a
marina-sheigets <sejmar1705@gmail.com>
2025-03-05 22:15:50 +0200

    fix: hide auth popup in active page

--- src/services/auth/auth.service.ts
+++ src/services/auth/auth.service.ts
@@ -115,6 +115,8 @@ export class AuthService {
     }
 
     private closeAllSignInPopups() {
+        this.closeSignInPopup();
+
         if (isExtensionContext()) {
             this.extensionPageManager.sendMessageToBackground(BackgroundMessages.CloseAllSignInPopups);
             return;
