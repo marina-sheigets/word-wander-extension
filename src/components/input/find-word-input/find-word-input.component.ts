f3640a72bf34ad86d6506d4ec707c706c9125691
marina-sheigets <sejmar1705@gmail.com>
2025-03-07 21:50:02 +0200

    fix: clear input if settings were changed

--- src/components/input/find-word-input/find-word-input.component.ts
+++ src/components/input/find-word-input/find-word-input.component.ts
@@ -2,14 +2,21 @@ import { singleton } from "tsyringe";
 import { i18nKeys } from "../../../services/i18n/i18n-keys";
 import { I18nService } from "../../../services/i18n/i18n.service";
 import { InputComponent } from "../input.component";
+import { SettingsService } from "../../../services/settings/settings.service";
+import { SettingsNames } from "../../../constants/settingsNames";
 
 @singleton()
 export class FindWordInputComponent extends InputComponent {
     constructor(
         protected i18nService: I18nService,
+        protected settingsService: SettingsService,
     ) {
         super(i18nService);
 
         this.setInputSettings('text', i18nKeys.EnterWord);
+
+        this.settingsService.subscribe(SettingsNames.User, () => {
+            this.clear();
+        });
     }
 }
