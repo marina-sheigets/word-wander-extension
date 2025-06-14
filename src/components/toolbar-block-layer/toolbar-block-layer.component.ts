160277df90e78daf2d32044c2b1940b275b62ce5
marina-sheigets <sejmar1705@gmail.com>
2025-06-15 00:10:40 +0300

    fix: toolbar block layer

--- src/components/toolbar-block-layer/toolbar-block-layer.component.ts
+++ src/components/toolbar-block-layer/toolbar-block-layer.component.ts
@@ -3,16 +3,15 @@ import { i18nKeys } from '../../services/i18n/i18n-keys';
 import { I18nService } from '../../services/i18n/i18n.service';
 import { TOOLBAR_MODE, ToolbarService } from '../../services/toolbar/toolbar.service';
 import { BaseComponent } from '../base-component/base-component';
-import { ButtonComponent } from '../button/button.component';
 import * as styles from './toolbar-block-layer.component.css';
 
 @singleton()
 export class ToolbarBlockLayerComponent extends BaseComponent {
     label = document.createElement('div');
+
     constructor(
         protected toolbarService: ToolbarService,
         protected i18n: I18nService,
-        protected learnWordsButton: ButtonComponent
     ) {
         super(styles);
 
@@ -23,13 +22,11 @@ export class ToolbarBlockLayerComponent extends BaseComponent {
             this.label.innerText = value;
         });
 
-        this.learnWordsButton.addButtonName(i18nKeys.GoToTrainings)
 
         this.hide();
 
         this.rootElement.append(
             this.label,
-            this.learnWordsButton.rootElement
         );
         this.rootElement.addEventListener('dblclick', this.toolbarService.setToolbarMode.bind(this.toolbarService, TOOLBAR_MODE.MINIMIZED));
     }
